1899592c3460754f7afbe303a1816fec0d88eb66
Mitama-san <106978598+Mitama-san@users.noreply.github.com>
2023-07-19 19:18:15 +0100

    adding latex support + other functions for bot

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 import discord
 import deepl
-import openai
+from music_links import music_tracks
+from discord.ext import commands
+import random
+from latex import build_pdf
+from sympy import sympify
+import os
+
+auth_key = "DEEPL_TOKEN"
+translator = deepl.Translator(auth_key)
+
+import discord
+import deepl
 from discord.ext import commands
 
-auth_key = "AUTH_KEY"
-translator = deepl.Translator(auth_key)
-openai.api_key = "API_KEY"
-
 def run_discord_bot():
-    TOKEN = "TOKEN"
+    TOKEN = "DISCORD_TOKEN"
     intents = discord.Intents.default()
     intents.message_content = True
     bot = commands.Bot(command_prefix='!', intents=intents)
@@ -27,57 +34,26 @@ def run_discord_bot():
         translation = translator.translate_text(text, target_lang='EN-GB')
         await ctx.send(translation)
 
-    @bot.event
-    async def on_message(message):
-        if message.author == bot.user:
-            return
+    @bot.command()
+    async def jukebox(ctx):
+        song_name, song_link = random.choice(list(music_tracks.items()))
+        await ctx.send(f"Now playing {song_name} \n" + song_link)
 
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-        print(f'{username} said: "{user_message}" ({channel})')
+    @bot.command()
+    async def latex(ctx, *, equation):
+        try:
+            expression = sympify(equation)
+            result = expression.evalf()
+            pdf = build_pdf(equation)
+            pdf.save_to("/path/to/temp.pdf")
+            await ctx.send(f"Result: {result}")
+            await ctx.send(file=discord.File("/path/to/temp.pdf"))
+            os.remove("/path/to/temp.pdf")
+        except Exception as e:
+            await ctx.send(f"Error: {str(e)}")
 
-        if user_message[0] == "?":
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
-
-        await bot.process_commands(message)
 
     bot.run(TOKEN)
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = await generate_response(user_message)
-        if response:
-            await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as error:
-        print(error)
-
-async def generate_response(user_message):
-    # Set up your GPT-3.5 parameters
-    messages = [
-        {"role": "system", "content": "You are a helpful language learning assistant."},
-        {"role": "user", "content": user_message}
-    ]
-
-    # Add prompts for personality
-    prompts = []
-
-    # Add prompts to the messages
-    for prompt in prompts:
-        messages.append({"role": "system", "content": prompt})
-
-    # Generate a response using GPT-3.5
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        max_tokens=50,
-        n=1,
-        temperature=0.7
-    )
-
-    return response.choices[0].message.content.strip()
 
 run_discord_bot()
